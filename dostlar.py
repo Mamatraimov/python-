7502c95860ad9bae0ba1167eac09666cb8070cb0
Mamatraimov <100739558+Mamatraimov@users.noreply.github.com>
2022-03-02 07:49:04 +0500

    zor dastur

---
+++ dostlar.py
@@ -0,0 +1,8 @@
+while True:
+    print("dostlaringizni ismini kiriting  ")
+    a= input("Asliddin dostlaringizni ismini kiriting:  ")
+    b = ('abror', 'nodir', 'adham', 'bohodir' , 'sardor')
+    if a in b:
+        print(a.title(), 'meni chin dostim \n')
+    else:
+        print(a,"meni bunaqa dostdim yuq \n")
